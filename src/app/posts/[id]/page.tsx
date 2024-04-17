1f7ef73ebf4f134a8e688873f41538956004f5b5
jakelee <freejak5520@gmail.com>
2024-04-17 16:21:51 +0900

    게시글 상세페이지, 스토리 작성

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { apiFetch } from "@/lib/getFetch";
 import MarkdownContent from "../_components/MarkdownContent";
+import { format } from "date-fns";
 
 const PostDetail = async ({ params }: { params: { id: string } }) => {
   const response = await apiFetch(`/api/posts/${params.id}`);
@@ -9,12 +10,15 @@ const PostDetail = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div>
-      <div className="text-3xl font-bold">{data.title}</div>
+      <div className="text-3xl font-bold mb-9 text-gray-950 dark:text-gray-50">
+        {data.title}
+      </div>
+      <div className="mb-8 text-sm text-gray-500 dark:text-gray-400 flex justify-end">
+        {format(data.created_at, "PPP")}
+      </div>
       <div>
         <MarkdownContent content={data.content} />
       </div>
-      <div>{new Date(data.created_at).toLocaleString()}</div>
-      <div>{new Date(data.updated_at).toLocaleString()}</div>
     </div>
   );
 };
